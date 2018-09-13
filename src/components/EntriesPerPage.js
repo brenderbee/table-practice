278d9ecae91a9da93898e3a37c4fe8872ae30664
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-12 23:02:38 -0700

    Add basic select box to EntriesPerPage component.

--- src/components/EntriesPerPage.js
+++ src/components/EntriesPerPage.js
@@ -3,7 +3,12 @@ import React from 'react';
 function EntriesPerPage() {
   return (
     <div>
-      <p>EntriesPerPage</p>
+      <p>entries per page</p>
+      <select>
+        <option value="All">All</option>
+        <option value="5">5</option>
+        <option value="10">10</option>
+      </select>
     </div>
   );
 }
