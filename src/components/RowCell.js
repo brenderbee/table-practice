e5063a9b9256527546c04015901ae6832332c968
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-12 21:22:30 -0700

    Create stylesheet for RowCell and import to RowCell component.

--- src/components/RowCell.js
+++ src/components/RowCell.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import './../assets/css/RowCell.css';
 import PropTypes from 'prop-types';
 
 class RowCell extends Component {
   render() {
     return (
-      <div>{this.props.cell}</div>
+      <div className="row-cell">
+        <p>{this.props.cell}</p>
+      </div>
     );
   }
 }
