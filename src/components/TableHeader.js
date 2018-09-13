5ec099b371af6d68eedfa10b7b97b3a6b591f8fb
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-12 21:41:12 -0700

    Change TableHeader to functional component.

--- src/components/TableHeader.js
+++ src/components/TableHeader.js
@@ -3,19 +3,17 @@ import ColumnHeader from './ColumnHeader';
 import './../assets/css/TableHeader.css';
 import PropTypes from 'prop-types';
 
-class TableHeader extends Component {
-  render() {
-    return (
-      <div className="table-header">
-        {this.props.columnList.map((header, index) =>
-          <ColumnHeader
-            key={index}
-            header={header}
-          />
-        )}
-      </div>
-    );
-  }
+function TableHeader(props) {
+  return (
+    <div className="table-header">
+      {props.columnList.map((header, index) =>
+        <ColumnHeader
+          key={index}
+          header={header}
+        />
+      )}
+    </div>
+  );
 }
 
 TableHeader.propTypes = {
