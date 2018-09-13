81d2b0539a39b5abb6d98c819536d2774a3af37c
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-12 19:57:04 -0700

    Create SortArrow component and import to ColumnHeader.

--- src/components/ColumnHeader.js
+++ src/components/ColumnHeader.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import SortArrow from './SortArrow';
 
 class ColumnHeader extends Component {
   render() {
     return (
       <div>
         <h2>ColumnHeader</h2>
+        <SortArrow />
       </div>
     );
   }
---
+++ src/components/SortArrow.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SortArrow extends Component {
+  render() {
+    return (
+      <div>
+        <p>arrow</p>
+      </div>
+    );
+  }
+}
+
+export default SortArrow;
