4d7d549a1bc2f703fc28e39a23b216b6bb3fba02
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-12 22:57:10 -0700

    Create stylesheet for SortArrow.

--- src/components/SortArrow.js
+++ src/components/SortArrow.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './../assets/css/SortArrow.css';
 import doubleArrow from './../assets/icons/svg/004-double-arrow.svg'
 
 function SortArrow() {
   return (
-    <div>
+    <div className="sort-arrow">
       <img src={doubleArrow} height="15" alt="Double sort arrow"/>
     </div>
   );
