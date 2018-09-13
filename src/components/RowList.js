6093375f640ed4e4b40a7131a00a40a1f96e6e6a
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-12 21:49:55 -0700

    Change RowList to functional component.

--- src/components/RowList.js
+++ src/components/RowList.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Row from './Row';
 import PropTypes from 'prop-types';
 
-class RowList extends Component {
-  render() {
-    return (
-      <div>
-        {this.props.rowList.map((rowData, index) =>
-          <Row
-            key={index}
-            rowData={rowData}
-          />
-        )}
-      </div>
-    );
-  }
+function RowList(props) {
+  return (
+    <div>
+      {props.rowList.map((rowData, index) =>
+        <Row
+          key={index}
+          rowData={rowData}
+        />
+      )}
+    </div>
+  );
 }
 
 RowList.propTypes = {
