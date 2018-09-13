cf312c4227c3083a4c463c365e5ffa79f230f5c0
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-12 22:51:57 -0700

    Import doubleArrow svg to SortArrow component.

--- src/components/SortArrow.js
+++ src/components/SortArrow.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import doubleArrow from './../assets/icons/svg/004-double-arrow.svg'
 
 function SortArrow() {
   return (
     <div>
-      <p>arrow</p>
+      <img src={doubleArrow} height="15" alt="Double sort arrow"/>
     </div>
   );
 }
