6aeb689ff79bd372192122669b2fd7f4dcd0b7cd
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-12 20:18:20 -0700

    Create Table.css file and import to Table component.

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,6 +3,7 @@ import { jsonResponse } from './../fakeData';
 import TableHeader from './TableHeader';
 import RowList from './RowList';
 import TableFooter from './TableFooter';
+import './../assets/css/Table.css';
 
 const rows = jsonResponse.data;
 const columns = jsonResponse.columns;
@@ -10,7 +11,7 @@ const columns = jsonResponse.columns;
 class Table extends Component {
   render() {
     return (
-      <div>
+      <div class="table">
         <TableHeader />
         <RowList />
         <TableFooter />
