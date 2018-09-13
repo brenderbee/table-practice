ec339f1ed7976831efa021e0245b7a4132135edd
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-12 20:12:05 -0700

    Create NavNumber component and import to EntriesNav.

--- src/components/EntriesNav.js
+++ src/components/EntriesNav.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import NavNumber from './NavNumber';
 
 class EntriesNav extends Component {
   render() {
     return (
       <div>
-        <p>EntriesNav</p>
+        <NavNumber />
       </div>
     );
   }
---
+++ src/components/NavNumber.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NavNumber extends Component {
+  render() {
+    return (
+      <div>
+        <p>1</p>
+      </div>
+    );
+  }
+}
+
+export default NavNumber;
