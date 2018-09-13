f4056252813ad3a97a750b6385ad07dea8434a5d
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-12 21:51:28 -0700

    Change RowCell to functional component.

--- src/components/RowCell.js
+++ src/components/RowCell.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './../assets/css/RowCell.css';
 import PropTypes from 'prop-types';
 
-class RowCell extends Component {
-  render() {
-    return (
-      <div className="row-cell">
-        <p>{this.props.cell}</p>
-      </div>
-    );
-  }
+function RowCell(props) {
+  return (
+    <div className="row-cell">
+      <p>{props.cell}</p>
+    </div>
+  );
 }
 
 RowCell.propTypes = {
