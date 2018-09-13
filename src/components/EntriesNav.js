9e6b98f30b5d6c5f7c453829d11a4b53a6af95fb
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-12 19:44:37 -0700

    Create EntriesNav and import to TableFooter.

---
+++ src/components/EntriesNav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EntriesNav extends Component {
+  render() {
+    return (
+      <div>
+        <p>EntriesNav</p>
+      </div>
+    );
+  }
+}
+
+export default EntriesNav;
--- src/components/TableFooter.js
+++ src/components/TableFooter.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import EntriesPerPage from './EntriesPerPage';
+import EntriesNav from './EntriesNav';
 
 class TableFooter extends Component {
   render() {
     return (
       <div>
         <EntriesPerPage />
+        <EntriesNav />
       </div>
     );
   }
