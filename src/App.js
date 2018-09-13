940a1de6aa6331cdcf5c6122aa81b072b8b2b483
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-12 18:33:14 -0700

    Import Table component to App.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
+import Table from './components/Table';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Table />
       </div>
     );
   }
