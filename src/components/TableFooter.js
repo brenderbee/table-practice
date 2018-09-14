dfe6f6fc7e8d4ee73b74241d3928c481f8e2e74a
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-13 20:10:18 -0700

    Pass onChangeEntries() to EntriesPerPage component.

--- src/components/TableFooter.js
+++ src/components/TableFooter.js
@@ -2,14 +2,21 @@ import React from 'react';
 import EntriesPerPage from './EntriesPerPage';
 import EntriesNav from './EntriesNav';
 import './../assets/css/TableFooter.css';
+import PropTypes from 'prop-types';
 
 function TableFooter(props) {
   return (
     <div className="table-footer">
-      <EntriesPerPage />
+      <EntriesPerPage
+        onChangeEntries={props.onChangeEntries}
+      />
       <EntriesNav />
     </div>
   );
 }
+
+TableFooter.propTypes = {
+  onChangeEntries: PropTypes.func
+};
 
 export default TableFooter;
