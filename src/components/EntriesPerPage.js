3b6a38a79c84f0d53d4fed3833e89477ff01d18d
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-13 20:23:00 -0700

    Add handleSendEntries() and onChange to select box that triggers handleChangeEntries in Table component.

--- src/components/EntriesPerPage.js
+++ src/components/EntriesPerPage.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './../assets/css/EntriesPerPage.css';
+import PropTypes from 'prop-types';
 
-function EntriesPerPage() {
+function EntriesPerPage(props) {
+
+  function handleSendEntries() {
+    props.onChangeEntries();
+  }
+
   return (
     <div className="entries-per-page">
       <p>entries per page</p>
-      <select>
+      <select onChange={handleSendEntries}>
         <option value="All">All</option>
         <option value="5">5</option>
         <option value="10">10</option>
@@ -13,5 +19,9 @@ function EntriesPerPage() {
     </div>
   );
 }
+
+EntriesPerPage.propTypes = {
+  onChangeEntries: PropTypes.func
+};
 
 export default EntriesPerPage;
