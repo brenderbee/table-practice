f95752db8d7fdd38f225e4b670655ef15cbd5097
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-14 07:49:20 -0700

    Add handleSendNewPage() and verify that it sends data to Table.

--- src/components/NavNumber.js
+++ src/components/NavNumber.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './../assets/css/NavNumber.css';
+import PropTypes from 'prop-types';
 
-function NavNumber() {
+function NavNumber(props) {
+
+  function handleSendNewPage(number) {
+    props.onChangeCurrentPage(parseInt(number, 10))
+  }
+
   return (
-    <div className="nav-number">
+    <div
+      className="nav-number"
+      onClick={() => handleSendNewPage(1)}
+    >
       1
     </div>
   );
 }
+
+NavNumber.propTypes = {
+  onChangeCurrentPage: PropTypes.func
+};
 
 export default NavNumber;
