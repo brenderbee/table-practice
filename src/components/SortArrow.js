b269c7533ea3f0a3ef3d58f6f3133f535d5c694f
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-13 19:18:02 -0700

    Import sort arrow svgs. Add renderSortArrow function to render arrows based on state. All arrows change. Need 'active sort' property.

--- src/components/SortArrow.js
+++ src/components/SortArrow.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import './../assets/css/SortArrow.css';
-import doubleArrow from './../assets/icons/svg/004-double-arrow.svg'
+import doubleArrow from './../assets/icons/svg/004-double-arrow.svg';
+import ascArrow from './../assets/icons/svg/006-up-arrow.svg';
+import descArrow from './../assets/icons/svg/005-down-arrow.svg';
+import PropTypes from 'prop-types';
 
-function SortArrow() {
+function SortArrow(props) {
+
+  function renderSortArrow(state) {
+    if (state === null) {
+      return <img src={doubleArrow} height="15" alt="Double sort arrow"/>;
+    } else if (state === 'asc') {
+      return <img src={ascArrow} height="15" alt="Ascending sort arrow"/>;
+    } else {
+      return <img src={descArrow} height="15" alt="Descending sort arrow"/>;
+    }
+  }
+
   return (
     <div className="sort-arrow">
-      <img src={doubleArrow} height="15" alt="Double sort arrow"/>
+      {renderSortArrow(props.sortOrder)}
     </div>
   );
 }
+
+SortArrow.propTypes = {
+  sortOrder: PropTypes.string
+};
+
 
 export default SortArrow;
